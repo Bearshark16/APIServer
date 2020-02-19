53348af3b58227fc281d9cefe7af4dbab84f73a9
Bearshark16 <54946080+Bearshark16@users.noreply.github.com>
2020-02-19 14:51:50 +0100

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIServer.Controllers
+{
+    [Route("api/pokemon")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        public static List<Pokemon> pokemons = new List<Pokemon>()
+        {
+            new Pokemon() {Name = "EKENS", Strength = 33},
+            new Pokemon() {Name = "CHARMANDER", Strength = 100},
+            new Pokemon() {Name = "EVEE", Strength = 300}
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            /*Pokemon p = new Pokemon();
+            p.Name = "PIKACHU";
+            p.Strength = "67";*/
+
+            return Ok(pokemons);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Pokemon newPokemon)
+        {
+            pokemons.Add(newPokemon);
+            Console.WriteLine("Added " + newPokemon.Name);
+
+            return Created("", newPokemon);
+        }
+
+    }
+}
---
+++ Pokemon.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServer
+{
+    public class Pokemon
+    {
+        private string name;
+        private int strength;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = NameToUpper(value);
+            }
+        }
+        public int Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                strength = value;
+            }
+        }
+
+        public static string NameToUpper(string name)
+        {
+            List<string> split = new List<string>() { };
+
+            char first;
+            string upper;
+            string result;
+
+            name = name.ToLower();
+
+            if (name.Contains(" ") || name.Contains('-'))
+            {
+                string[] stringSplit = null;
+
+                if (name.Contains(" "))
+                {
+                    stringSplit = name.Split(' ');
+                }
+                else if (name.Contains('-'))
+                {
+                    stringSplit = name.Split('-');
+                }
+
+                foreach (string x in stringSplit)
+                {
+                    first = x[0];
+                    upper = first.ToString().ToUpper();
+                    string nameCaps = upper + x.Remove(0, 1);
+                    split.Add(nameCaps);
+                }
+
+                result = string.Join(" ", split.ToArray());
+            }
+            else
+            {
+                first = name[0];
+                upper = first.ToString().ToUpper();
+                result = upper + name.Remove(0, 1);
+            }
+
+            return result;
+        }
+    }
+}
